becfcc220ba228600cdd99022434066647f0ee03
hans <hanssong197@gamil.com>
2025-10-17 13:22:49 +0100

    TDD start

--- tdd-assessment/src/main/java/calculator.java
+++ tdd-assessment/src/main/java/calculator.java
@@ -1,26 +1,7 @@
 package src.main.java;
 
-public class calculator{
+public class Calculator{
 
-public int add(int a, int b)
-{
-    return a + b;
-}
-public int subtract(int a, int b) //test
-{
-    return a - b;
-}
-public int multiply(int a, int b)
-{
-    return a * b;
-}
-public int divide(int a, int b)
-{
-    if(b==0){
-        throw new IllegalArgumentException("Cannot divide by zero");
-    }
-    else{
-        return a / b;
-    }
-}
+
+
 }
--- tdd-assessment/src/test/java/calculatortest.java
+++ tdd-assessment/src/test/java/calculatortest.java
@@ -2,14 +2,13 @@ package src.test.java;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import src.main.java.calculator;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import src.main.java.Calculator;
 
-public class calculatortest {
-    @Test
-    void testadd() {
-        calculator c = new calculator();
-        assertEquals(5, c.add(2, 3)); 
-    }
-    
+class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
 
 }
