38f89051c67d7041415bf29a0e7e92bb5ac574a2
Hans <hanssong197@gmail.com>
2025-10-17 15:04:57 +0100

    write a divide() method

--- tdd-assessment/src/main/java/Calculator.java
+++ tdd-assessment/src/main/java/Calculator.java
@@ -14,7 +14,12 @@ public class Calculator{
         return a * b;
     }
 
-
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 
 
 }
